750fe93bf0486281f92dc1e9f44a17f1cb1290cd
a.lobachyov <a.lobachyov@zebrains.team>
2019-11-27 11:32:32 +0400

    update packages, add prettier

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Button } from 'react-native-material-buttons';
+import React, {PureComponent} from 'react';
+import {Button} from 'react-native-material-buttons';
 
 import styles from './styles';
 
@@ -25,23 +25,21 @@ export default class DropdownItem extends PureComponent {
   }
 
   onPress() {
-    let { onPress, index } = this.props;
+    let {onPress, index} = this.props;
 
-    if ('function' === typeof onPress) {
+    if (typeof onPress === 'function') {
       onPress(index);
     }
   }
 
   render() {
-    let { children, style, index, ...props } = this.props;
+    let {children, style, index, ...props} = this.props;
 
     return (
       <Button
         {...props}
-
         style={[styles.container, style]}
-        onPress={this.onPress}
-      >
+        onPress={this.onPress}>
         {children}
       </Button>
     );
